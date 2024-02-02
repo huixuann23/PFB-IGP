c8d3456938f9bc41ae7c5d143a19038ec36cc200
huixuann23 <s10257984@connect.np.edu.sg>
2024-02-02 11:34:23 +0800

    IGP FINALS

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import overheads, cash_on_hand, profit_and_loss
+import overheads, cash_on_hand, profit_loss
 from pathlib import Path
 
 def main():
     overheads.overhead()
     cash_on_hand.Cash_On_Hand()
-    profit_and_loss.Profit_And_Loss()
+    profit_loss.Profit_And_Loss()
 
 main()
